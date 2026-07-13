5e880fe6a9c1d1ea135ef0ab26b9ae422f3f927f
Student <Student@top.ru>
2026-07-13 17:38:08 +0300

    Implementation of practice 1 & pydantic validation

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, validator
 
 app = FastAPI()
 
@@ -14,7 +14,18 @@ app = FastAPI()
 
 class UserModel(BaseModel):
     id: str
-    name: str
+    name: str = Field(
+        "Nobody",
+        min_length=3,
+        max_length=20,
+        description="User's name"
+    )
+    
+    @validator("name")
+    def check_name_not_empty(self, v):
+        if not v.strip():
+            raise ValueError("Name must contain something")
+        return v
 
 users = [
     {
@@ -39,14 +50,22 @@ def get_user_by_id(id):
 
 @app.post("/users", response_model=UserModel, status_code=201)
 def create_user(user: UserModel):
-    user_dict = {
-        "id": user.id,
-        "name": user.name
-    }
-    users.append(user_dict)
+    user_data = user.dict()
     
-    return user_dict
+    for existing_user in users:
+        if existing_user["id"] == user_data["id"]:
+            raise HTTPException(status_code=400, detail="User elready exists")
+    
+    users.append(user_data)
+    return user_data
 
 # Практика 1: ошибка при создании
 # Если при создании пользователя пользователь с таким id уже существует 
 #  Вернуть ошибку 400 с комментарием "User already exists"
+
+# Практика 2: пароль
+# Добавьте поле password со следующими ограничениями:
+# - не меньше 8 символов
+# - не больше 30 символов
+# - хотя бы один символ верхнего регистра, один - нижнего
+#   и одна цифра
